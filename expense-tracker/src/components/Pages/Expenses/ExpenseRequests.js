8eb34fbe268f1fc46f2124d2f41c5e798548157e
MilanTibadiya <tibadiyamilan@gmail.com>
2023-04-13 17:08:58 +0530

    firebase update

--- expense-tracker/src/components/Pages/Expenses/ExpenseRequests.js
+++ expense-tracker/src/components/Pages/Expenses/ExpenseRequests.js
@@ -4,7 +4,7 @@ import { expensesActions } from "../../../store/ExpenseSlicer";
 
 
 export async function getExpenses(dispatch) {
-    const res = await axios.get(`https://signup-and-authentication-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}.json`);
+    const res = await axios.get(`https://expense-30064-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}.json`);
     const data = [];
     let sum = 0;
       for (let key in res.data) {
@@ -17,16 +17,16 @@ export async function getExpenses(dispatch) {
 }
 
 export async function deleteExpanse(id, dispatch) {
-    const res = await axios.delete(`https://signup-and-authentication-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}/${id}.json`)
+    const res = await axios.delete(`https://expense-30064-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}/${id}.json`)
     getExpenses(dispatch);
 }
 
 export async function editExpense(id, item, dispatch) {
-    const res = await axios.put(`https://signup-and-authentication-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}/${id}.json`, item);
+    const res = await axios.put(`https://expense-30064-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}/${id}.json`, item);
     await getExpenses(dispatch);
 }
 
 export async function postExpenses(item, dispatch){
-    const res = await axios.post(`https://signup-and-authentication-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}.json`,item);
+    const res = await axios.post(`https://expense-30064-default-rtdb.firebaseio.com/expenses${localStorage.getItem("userEmail").split("@")[0]}.json`,item);
     getExpenses(dispatch)
 }
